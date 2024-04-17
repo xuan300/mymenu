1b06b8ef1ce98bfd36fcd38c75bbb87725822ebf
xuan <111b13876@mailst.cjcu.edu.tw>
2024-04-17 13:47:26 +0800

    Initial commit1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+import static com.example.myapplication.R.string.yummy;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button systemButton = findViewById(R.id.systembutton);
+        systemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMultiChoiceDialog();
+            }
+        });
+    }
+
+    private void showMultiChoiceDialog() {
+        final String[] items = new String[]{getString(yummy), getString(R.string.sausage), getString(R.string.fruit), getString(R.string.banana), getString(R.string.chicken)};
+        final boolean[] checkedItems = {false, false, false, false, false};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.selected_items); // 使用新的字符串資源
+        builder.setMultiChoiceItems(items, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                checkedItems[which] = isChecked;
+            }
+        });
+        builder.setPositiveButton("確認", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Handle the selected items
+            }
+        });
+        builder.setNegativeButton("離開", null);
+        builder.show();
+    }
+}
